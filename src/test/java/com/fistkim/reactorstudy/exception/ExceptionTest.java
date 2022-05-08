c9df39615661f5d2369c05a442ebd3de2399dabd
fistkim101 <fistkim101@gmail.com>
2022-05-08 18:06:52 +0900

    ~onErrorResuem

---
+++ src/test/java/com/fistkim/reactorstudy/exception/ExceptionTest.java
@@ -0,0 +1,80 @@
+package com.fistkim.reactorstudy.exception;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+
+public class ExceptionTest {
+
+    @Test
+    void terminateCheck() {
+        Flux<Integer> numbers = Flux.fromIterable(List.of(1, 2, 3));
+        Flux<Integer> numbersWithException = numbers.concatWith(Mono.error(RuntimeException::new)).concatWith(Mono.just(4)).log();
+
+        StepVerifier.create(numbersWithException).expectNext(1, 2, 3).expectError().verify();
+    }
+
+    @Test
+    void onErrorReturnTest() {
+        Flux<Integer> numbers = Flux.fromIterable(List.of(1, 2, 3)).concatWith(Mono.error(new RuntimeException())).onErrorReturn(4).log();
+
+        StepVerifier.create(numbers).expectNext(1, 2, 3, 4).verifyComplete();
+    }
+
+    @Test
+    void onErrorReturnTest_2() {
+        Flux<Integer> numbers_1 = Flux.fromIterable(List.of(1, 2, 3));
+        Flux<Integer> numbers_2 = Flux.fromIterable(List.of(5, 6, 7));
+
+        Flux<Integer> mergedNumbers = numbers_1.concatWith(Mono.error(new RuntimeException())).onErrorReturn(4).concatWith(numbers_2).log();
+
+        StepVerifier.create(mergedNumbers).expectNext(1, 2, 3, 4, 5, 6, 7).verifyComplete();
+    }
+
+    @Test
+    void onErrorResumeTest_1() {
+        Flux<Integer> numbers = Flux.fromIterable(List.of(1, 2, 3));
+        Flux<Integer> target = numbers.concatWith(Mono.error(new RuntimeException())).onErrorResume(exception -> {
+            System.out.println(exception.getClass().getName());
+            return Flux.fromIterable(List.of(4, 5, 6));
+        }).log();
+
+        StepVerifier.create(target).expectNext(1, 2, 3, 4, 5, 6).verifyComplete();
+    }
+
+    @Test
+    void onErrorResumeTest_2() {
+        Flux<Integer> numbers_1 = Flux.fromIterable(List.of(1, 2, 3));
+        Flux<Integer> numbers_2 = Flux.fromIterable(List.of(10, 11, 12));
+        Flux<Integer> target = numbers_1
+                .concatWith(Mono.error(new RuntimeException()))
+                .concatWith(numbers_2).onErrorResume(exception -> {
+                    System.out.println(exception.getClass().getName());
+                    return Flux.fromIterable(List.of(4, 5, 6));
+                }).log();
+
+        StepVerifier.create(target)
+                .expectNext(1, 2, 3, 4, 5, 6)
+                .verifyComplete();
+    }
+
+    @Test
+    void onErrorResumeTest_3() {
+        Flux<Integer> numbers_1 = Flux.fromIterable(List.of(1, 2, 3));
+        Flux<Integer> numbers_2 = Flux.fromIterable(List.of(10, 11, 12));
+        Flux<Integer> target = numbers_1
+                .concatWith(Mono.error(new RuntimeException()))
+                .concatWith(numbers_2)
+                .onErrorResume(exception -> {
+                    System.out.println(exception.getClass().getName());
+                    return Flux.empty();
+                }).log();
+
+        StepVerifier.create(target)
+                .expectNext(1, 2, 3)
+                .verifyComplete();
+    }
+}
--- src/test/java/com/fistkim/reactorstudy/sideeffect/SideEffectTest.java
+++ src/test/java/com/fistkim/reactorstudy/sideeffect/SideEffectTest.java
@@ -1,2 +1,34 @@
-package com.fistkim.reactorstudy.sideeffect;public class SideEffectTest {
+package com.fistkim.reactorstudy.sideeffect;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+
+public class SideEffectTest {
+
+    @Test
+    void sideEffect() {
+        Flux<Integer> numbers = Flux.fromIterable(List.of(1, 2, 3))
+                .doOnEach(number -> System.out.println("doOnEach : " + number))
+                .doOnNext(number -> System.out.println("doOnNext : " + number))
+                .doOnSubscribe(number -> System.out.println("doOnSubscribe : " + number))
+                .log()
+                .doOnComplete(() -> System.out.println("doOnComplete"));
+
+        StepVerifier.create(numbers)
+                .assertNext(number -> {
+                    Assertions.assertEquals(1, number);
+                })
+                .assertNext(number -> {
+                    Assertions.assertEquals(2, number);
+                })
+                .assertNext(number -> {
+                    Assertions.assertEquals(3, number);
+                })
+                .verifyComplete();
+    }
+
 }
